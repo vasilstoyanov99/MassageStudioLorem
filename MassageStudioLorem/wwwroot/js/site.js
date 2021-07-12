b5229bc0cacbf8975f2b9f62811ad9185edf0459
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-07-13 02:50:16 +0300

    Added partially done booking table + layout pic

--- MassageStudioLorem/wwwroot/js/site.js
+++ MassageStudioLorem/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*
 
-// Write your JavaScript code.
+    1. Booking table
+
+    */
+
+(function ($) {
+    "use strict";
+    $('.column100').on('mouseover', function () {
+        var table1 = $(this).parent().parent().parent();
+        var table2 = $(this).parent().parent();
+        var verTable = $(table1).data('vertable') + "";
+        var column = $(this).data('column') + "";
+
+        $(table2).find("." + column).addClass('hov-column-' + verTable);
+        $(table1).find(".row100.head ." + column).addClass('hov-column-head-' + verTable);
+    });
+
+    $('.column100').on('mouseout', function () {
+        var table1 = $(this).parent().parent().parent();
+        var table2 = $(this).parent().parent();
+        var verTable = $(table1).data('vertable') + "";
+        var column = $(this).data('column') + "";
+
+        $(table2).find("." + column).removeClass('hov-column-' + verTable);
+        $(table1).find(".row100.head ." + column).removeClass('hov-column-head-' + verTable);
+    });
+
+
+})(jQuery);
