325f79a004cb66cef8f09051b111f59c0ad2f41a
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-07-22 03:32:06 +0300

    Update HomeController.cs

--- MassageStudioLorem/Controllers/HomeController.cs
+++ MassageStudioLorem/Controllers/HomeController.cs
@@ -70,15 +70,8 @@
         //    });
         //}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => View();
     }
 }
