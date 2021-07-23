dcd73c237c7789b538bb60fcd4c0aecc612bffc9
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-07-23 04:00:44 +0300

    Added cosmetic changes

--- MassageStudioLorem/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MassageStudioLorem/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,15 +80,18 @@
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
+
                 if (result.RequiresTwoFactor)
                 {
                     return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
                 }
+
                 if (result.IsLockedOut)
                 {
                     _logger.LogWarning("User account locked out.");
