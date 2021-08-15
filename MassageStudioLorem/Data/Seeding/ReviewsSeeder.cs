a171172bfb0ce1a9ea326f6ce05f8855ca93b530
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-08-15 23:15:45 +0300

    Update ReviewsSeeder.cs

--- MassageStudioLorem/Data/Seeding/ReviewsSeeder.cs
+++ MassageStudioLorem/Data/Seeding/ReviewsSeeder.cs
@@ -14,25 +14,25 @@
             if (!data.Reviews.Any())
             {
                 var appointments = data.Appointments.ToList();
-                var selected = appointments[0];
-                appointments.Remove(selected);
 
                 for (int i = 0; i < appointments.Count; i++)
                 {
-                    var review = new Review()
-                    {
-                        ClientFirstName = appointments[i].ClientFirstName,
-                        ClientId = appointments[i].ClientId,
-                        Content = ReviewSeedData.DummyReviewContent,
-                        MasseurId = appointments[i].MasseurId,
-                        CreatedOn = appointments[i].Date
-                            .AddHours(appointments[i].Hour[1])
-                    };
-
                     if (i % 2 == 0)
+                    {
+                        var review = new Review()
+                        {
+                            ClientFirstName = appointments[i].ClientFirstName,
+                            ClientId = appointments[i].ClientId,
+                            Content = ReviewSeedData.DummyReviewContent,
+                            MasseurId = appointments[i].MasseurId,
+                            CreatedOn = appointments[i].Date
+                                .AddHours(appointments[i].Hour[1])
+                        };
+
                         appointments[i].IsUserReviewedMasseur = true;
 
-                    data.Reviews.Add(review);
+                        data.Reviews.Add(review);
+                    }
                 }
 
                 data.SaveChanges();
