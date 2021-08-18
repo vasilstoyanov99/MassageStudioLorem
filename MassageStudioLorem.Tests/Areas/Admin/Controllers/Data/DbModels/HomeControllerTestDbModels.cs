c1ac5523806cc0cd467b5a5eb985ef7118bd40cf
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-08-18 18:37:36 +0300

    Added tests for the Home Controller in the Admin area

---
+++ MassageStudioLorem.Tests/Areas/Admin/Controllers/Data/DbModels/HomeControllerTestDbModels.cs
@@ -0,0 +1,19 @@
+﻿namespace MassageStudioLorem.Tests.Areas.Admin.Controllers.Data.DbModels
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class HomeControllerTestDbModels
+    {
+        public static IdentityUser TestAdminUser => new()
+        {
+            Id = TestIdentityUserData.Id,
+            UserName = TestIdentityUserData.Username
+        };
+
+        public static class TestIdentityUserData
+        {
+            public const string Id = "TestId";
+            public const string Username = "TestUsername";
+        }
+    }
+}
---
+++ MassageStudioLorem.Tests/Areas/Admin/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿namespace MassageStudioLorem.Tests.Areas.Admin.Controllers
+{
+    using MassageStudioLorem.Areas.Admin.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+
+    using static MassageStudioLorem.Areas.Admin.AdminConstants;
+    using static Data.DbModels.HomeControllerTestDbModels;
+
+    public class HomeControllerTest
+    {
+        [Theory]
+        [InlineData("AdminUserName")]
+        public void ViewBagShouldContainsMasseurFullNameWhenLoggedIn
+            (string viewBagKey)
+            => MyMvc
+                .Controller<HomeController>()
+                .WithUser(u => u.InRole(AdminRoleName))
+                .WithData(TestAdminUser)
+                .Calling(c => c.Index())
+                .ShouldHave()
+                .ViewBag(viewBag => viewBag
+                    .ContainingEntry(viewBagKey, TestAdminUser.UserName))
+                .AndAlso()
+                .ShouldReturn()
+                .View();
+    }
+}
