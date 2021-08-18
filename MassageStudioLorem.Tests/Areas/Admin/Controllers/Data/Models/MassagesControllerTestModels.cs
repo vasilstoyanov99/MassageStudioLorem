dd79f4b5d71e76712eb78036ff23845580c7a23e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-08-18 22:28:50 +0300

    Added tests for the Massages Controller in the Admin area

---
+++ MassageStudioLorem.Tests/Areas/Admin/Controllers/Data/Models/MassagesControllerTestModels.cs
@@ -0,0 +1,51 @@
+﻿namespace MassageStudioLorem.Tests.Areas.Admin.Controllers.Data.Models
+{
+    using System.Collections.Generic;
+
+    using Services.Massages.Models;
+
+    using static DbModels.MassagesControllerTestDbModels;
+
+    public class MassagesControllerTestModels
+    {
+        public static MassageListingServiceModel TestMassageListingModel
+            => new()
+            {
+                Id = TestMassage.Id,
+                Name = TestMassage.Name,
+                ImageUrl = TestMassage.ImageUrl,
+                ShortDescription = TestMassage.ShortDescription
+            };
+
+        public static AllCategoriesQueryServiceModel AllCategoriesModel
+            => new()
+            {
+                CurrentPage = 1,
+                Id = TestCategory.Id,
+                Name = TestCategory.Name,
+                TotalCategories = 1,
+                Massages = new List<MassageListingServiceModel>() { TestMassageListingModel }
+            };
+
+        public static EditMassageFormModel EditMassageModel => new()
+        {
+            Id = TestMassage.Id,
+            ImageUrl = TestMassage.ImageUrl,
+            ShortDescription = TestMassage.ShortDescription,
+            LongDescription = TestMassage.LongDescription,
+            Name = TestMassage.Name,
+            Price = TestMassage.Price
+        };
+
+        public static AddMassageFormModel AddMassageModel => new()
+        {
+            Id = TestMassage.Id,
+            Name = TestMassage.Name,
+            CategoryId = TestCategory.Id,
+            ImageUrl = TestMassage.ImageUrl,
+            ShortDescription = TestMassage.ShortDescription,
+            LongDescription = TestMassage.LongDescription,
+            Price = TestMassage.Price
+        };
+    }
+}
--- MassageStudioLorem.Tests/Areas/Admin/Controllers/MassagesControllerTest.cs
+++ MassageStudioLorem.Tests/Areas/Admin/Controllers/MassagesControllerTest.cs
@@ -1,6 +1,174 @@
 ﻿namespace MassageStudioLorem.Tests.Areas.Admin.Controllers
 {
+    using System.Linq;
+
+    using MyTested.AspNetCore.Mvc;
+    using Shouldly;
+    using Xunit;
+
+    using MassageStudioLorem.Areas.Admin.Controllers;
+    using MassageStudioLorem.Data.Models;
+    using Services.Massages.Models;
+    using Services.SharedModels;
+
+    using static Data.DbModels.MassagesControllerTestDbModels;
+    using static Data.Models.MassagesControllerTestModels;
+    using static Global.GlobalConstants.Notifications;
+
     public class MassagesControllerTest
     {
+        [Fact]
+        public void AllShouldReturnViewWithValidData()
+        {
+            var expectedModel = AllCategoriesModel;
+
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestMassage, TestCategory)
+                .Calling(c => c.All(new AllCategoriesQueryServiceModel()
+                {
+                    CurrentPage = expectedModel.CurrentPage, 
+                    Id = expectedModel.Id, 
+                    Name = expectedModel.Name, 
+                    TotalCategories = expectedModel.TotalCategories
+                }))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+
+        [Fact]
+        public void DetailsShouldReturnViewWithValidData()
+        {
+            var expectedModel = new EditMassageDetailsServiceModel
+            {
+                Id = TestMassage.Id,
+                Name = TestMassage.Name,
+                ImageUrl = TestMassage.ImageUrl,
+                LongDescription = TestMassage.LongDescription,
+                Price = TestMassage.Price
+            };
+
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestMassage)
+                .Calling(c => c.Details(TestMassage.Id))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+
+        [Fact]
+        public void DeleteMassageShouldReturnViewWithValidData()
+        {
+            var expectedModel = new DeleteEntityServiceModel
+            {
+                Id = TestMassage.Id,
+                Name = TestMassage.Name,
+                CategoryName = TestCategory.Name,
+                EntityName = "Massage"
+            };
+
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestMassage, TestCategory)
+                .Calling(c => c.DeleteMassage(TestMassage.Id))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+
+        [Fact]
+        public void DeleteMassageShouldDeleteMassageWithValidDataAndReturnViewWithTempData
+            ()
+        {
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestMassage, TestCategory, TestUpcomingAppointment)
+                .Calling(c => c.Delete(TestMassage.Id))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Massage>(set => set.ShouldBeEmpty()))
+                .AndAlso()
+                .ShouldHave()
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(SuccessfullyDeletedMassageKey))
+                .AndAlso()
+                .ShouldReturn()
+                .View("All");
+        }
+
+        [Fact]
+        public void EditMassageShouldReturnViewWithValidData()
+        {
+            var expectedModel = EditMassageModel;
+
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestMassage)
+                .Calling(c => c.EditMassage(TestMassage.Id))
+                .ShouldReturn()
+                .View(expectedModel);
+        }
+
+        [Fact]
+        public void EditMassageShouldEditMassageWithValidDataAndReturnRedirectWithTempData
+            ()
+        {
+            var editedMassageModel = EditMassageModel;
+            editedMassageModel.Name = "New Test Massage Name";
+
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestMassage)
+                .Calling(c => c.Edit(editedMassageModel))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Massage>(set =>
+                    {
+                        var editedName = set.First().Name;
+                        editedName.ShouldNotBeSameAs(TestMassageData.Name);
+                    }))
+                .AndAlso()
+                .ShouldHave()
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(SuccessfullyEditedMassageKey))
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("All");
+        }
+
+        [Fact]
+        public void AddMassageShouldReturnViewWithValidData()
+        {
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestCategory)
+                .Calling(c => c.AddMassage())
+                .ShouldReturn()
+                .View();
+        }
+
+        [Fact]
+        public void AddMassageShouldAddMassageWithValidDataAndReturnRedirectWithTempData
+            ()
+        {
+            MyController<MassagesController>
+                .Instance()
+                .WithData(TestCategory)
+                .Calling(c => c.Add(AddMassageModel))
+                .ShouldHave()
+                .Data(data => data
+                    .WithSet<Massage>(set =>
+                    {
+                        var newMassageName = set.First().Name;
+                        newMassageName
+                            .ShouldBe(TestMassage.Name);
+                    }))
+                .AndAlso()
+                .ShouldHave()
+                .TempData(tempData => tempData
+                    .ContainingEntryWithKey(SuccessfullyAddedMassageKey))
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("All");
+        }
     }
 }
